f4cd1cddf136cce150a79a6f60e1b0ace666024e
Max Maldonado <nuup20@gmail.com>
2026-02-07 11:04:05 -0600

    update vertex

--- hcCore/include/hc/hcVertex.h
+++ hcCore/include/hc/hcVertex.h
@@ -6,7 +6,15 @@ namespace hc
 {
   struct HC_CORE_EXPORT Vertex
   {
-    Vertex() = default;
+    Vertex() :
+      position(0.0f, 0.0f, 0.0f),
+      normal(0.0f, 1.0f, 0.0f),
+      tangent(1.0f, 0.0f, 0.0f),
+      texCoord(0.0f, 0.0f),
+      color(1.0f, 1.0f, 1.0f, 1.0f)
+    {
+    }
+
     Vertex(
       const Vector3f& pos,
       const Vector3f& norm,
