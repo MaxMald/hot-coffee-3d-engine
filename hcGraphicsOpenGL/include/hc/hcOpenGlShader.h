39e3313467483e5c953f157ffb0d5410d1f177b5
Max Maldonado <nuup20@gmail.com>
2026-02-10 10:16:11 -0600

    add openg gl shader factory

--- hcGraphicsOpenGL/include/hc/hcOpenGlShader.h
+++ hcGraphicsOpenGL/include/hc/hcOpenGlShader.h
@@ -5,32 +5,47 @@
 
 namespace hc
 {
+  /**
+   * @brief Represents an OpenGL shader object.
+   */
   class OpenGlShader : public IShader
   {
   public:
+    /**
+     * @brief Constructs an OpenGlShader with the given stage type and source
+     * code.
+     *
+     * @param stageType The shader stage type (e.g., vertex, fragment).
+     * @param source The GLSL source code for the shader.
+     */
     OpenGlShader(shaderStageType::Type stageType, const String& source);
     ~OpenGlShader() override;
 
     /**
-     * @copydoc IShader::getStageType
+     * @brief Returns the stage type of the shader (vertex, fragment, etc.).
      */
     shaderStageType::Type getStageType() const override;
 
     /**
-     * @copydoc IShader::isCompiled
+     * @brief Checks if the shader has been successfully compiled.
      */
     bool isCompiled() const override;
 
     /**
-     * @copydoc IShader::compile
+     * @brief Compiles the shader from its source code.
      */
     void compile() override;
 
     /**
-     * @copydoc IShader::destroy
+     * @brief Destroys the shader and releases OpenGL resources.
      */
     void destroy() override;
 
+    /**
+     * @brief Returns the OpenGL shader object ID.
+     * 
+     * @return The OpenGL shader ID.
+     */
     GLuint getShaderId() const;
 
   private:
