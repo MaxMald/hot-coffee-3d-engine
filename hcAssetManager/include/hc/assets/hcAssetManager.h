427ca4a29c2086c661e1bcde7cd49c87cdca1d4b
Max Maldonado <nuup20@gmail.com>
2026-03-06 17:48:45 -0600

    Update asset manager

--- hcAssetManager/include/hc/assets/hcAssetManager.h
+++ hcAssetManager/include/hc/assets/hcAssetManager.h
@@ -7,21 +7,54 @@
 
 namespace hc
 {
+  /**
+   * Concrete implementation of the central asset manager.
+   *
+   * Owns and coordinates all specialized asset managers.
+   */
   class HC_ASSET_MANAGER_EXPORT AssetManager : public IAssetManager
   {
   public:
+    /** Constructs the asset manager and initializes all sub-managers */
     AssetManager();
-    virtual ~AssetManager();
 
+    /** Virtual destructor */
+    virtual ~AssetManager() = default;
+
+    /**
+     * Retrieves the model asset manager.
+     *
+     * @return Reference to the model asset manager instance
+     */
     IModelAssetManager& getModelAssetManager() override;
+
+    /**
+     * Retrieves the image asset manager.
+     *
+     * @return Reference to the image asset manager instance
+     */
     IImageAssetManager& getImageAssetManager() override;
+
+    /**
+     * Retrieves the material descriptor asset manager.
+     *
+     * @return Reference to the material descriptor asset manager
+     */
     IMaterialDescriptorAssetManager& getMaterialDescriptorAssetManager() override;
+
+    /**
+     * Clears all asset groups from all sub-managers.
+     */
     void clear() override;
+
+    /**
+     * Destroys the asset manager, clearing all assets and resources.
+     */
     void destroy() override;
 
   private:
+    MaterialDescriptorAssetManager m_materialDescriptorAssetManager;
     ModelAssetManager m_modelAssetManager;
     ImageAssetManager m_imageAssetManager;
-    MaterialDescriptorAssetManager m_materialDescriptorAssetManager;
   };
 }
