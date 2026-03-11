8b9add8f5ae08ffb40a0a4f00aa3cdbdd6a1cbc5
Max Maldonado <nuup20@gmail.com>
2026-03-11 12:49:10 -0600

    Update hcBitUtilities.h

--- hcUtilities/include/hc/utilities/hcBitUtilities.h
+++ hcUtilities/include/hc/utilities/hcBitUtilities.h
@@ -19,5 +19,20 @@ namespace hc
       UInt32 mask = -static_cast<Int32>(floatBits >> 31) | 0x80000000;
       return floatBits ^ mask;
     }
+
+    /**
+     * Flips the bits of a floating-point number to allow for correct sorting when
+     * treated as an integer.
+     * 
+     * @param value The floating-point number to flip.
+     *
+     * @return The flipped bits that can be used for sorting.
+     */
+    inline UInt32 floatFlip(float value)
+    {
+      UInt32 floatBits;
+      std::memcpy(&floatBits, &value, sizeof(float));
+      return floatFlip(floatBits);
+    }
   }
 }
