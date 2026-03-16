2778eb48dadecdc0f7b89da9f7ebdfa89d510604
Max Maldonado <nuup20@gmail.com>
2026-03-16 17:25:05 -0600

    Feature Component Serialization

--- hcCore/include/hc/scene/gameObject/components/hcIComponent.h
+++ hcCore/include/hc/scene/gameObject/components/hcIComponent.h
@@ -7,13 +7,33 @@ namespace hc
 {
   class GameObject;
 
-  class HC_CORE_EXPORT IComponent : public NonCopyable
+  /**
+   * @brief Interface for components that can be attached to game objects.
+   */
+  class HC_CORE_EXPORT IComponent : public NonCopyable, public ISerializable
   {
   public:
     virtual ~IComponent() = default;
 
+    /**
+     * Sets the game object this component is attached to.
+     * 
+     * @param gameObject Pointer to the parent game object.
+     */
     virtual void setGameObject(GameObject* gameObject) = 0;
+
+    /**
+     * Gets the game object this component is attached to.
+     * 
+     * @return Pointer to the parent game object.
+     */
     virtual GameObject* getGameObject() const = 0;
+
+    /**
+     * Gets the type of this component.
+     * 
+     * @return The component type identifier.
+     */
     virtual componentType::Type getType() const = 0;
 
   protected:
