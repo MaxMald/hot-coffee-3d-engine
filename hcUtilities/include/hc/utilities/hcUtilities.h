a774b57f06fb32af4fac94a4116ff772e9261119
Max Maldonado <nuup20@gmail.com>
2026-03-16 16:49:35 -0600

    Update hcUtilities.h

--- hcUtilities/include/hc/utilities/hcUtilities.h
+++ hcUtilities/include/hc/utilities/hcUtilities.h
@@ -30,3 +30,6 @@
 #include "hc/utilities/hcTime.h"
 #include "hc/utilities/hcClock.h"
 #include "hc/utilities/hcBitUtilities.h"
+#include "hc/utilities/io/hcBinaryReader.h"
+#include "hc/utilities/io/hcBinaryWriter.h"
+#include "hc/utilities/io/hcISerializable.h"
