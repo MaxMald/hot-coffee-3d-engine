b57dabcdb30569ed16f112397ea0fb89b4afcba5
Max Maldonado <nuup20@gmail.com>
2026-05-27 09:44:45 -0600

    update model loader

--- hcAssetManager/src/assets/model/hcAssimpMaterialDescriptorParser.cpp
+++ hcAssetManager/src/assets/model/hcAssimpMaterialDescriptorParser.cpp
@@ -10,16 +10,20 @@ namespace hc
     const aiMaterial* material
   )
   {
-    shadingType::Type type = DetermineShadingType(material);
+    shadingType::Type type = GetShadingTypeFromMaterial(material);
 
     switch (type)
     {
+    case shadingType::Unlit:
+      return ParseUnlitMaterialDescriptor(fileDirectory, material);
+    case shadingType::BlinnPhong:
+      return ParseBlinnPhongMaterialDescriptor(fileDirectory, material);
     default:
       return ParseUnlitMaterialDescriptor(fileDirectory, material);
     }
   }
 
-  shadingType::Type AssimpMaterialDescriptorParser::DetermineShadingType(const aiMaterial* material)
+  shadingType::Type AssimpMaterialDescriptorParser::GetShadingTypeFromMaterial(const aiMaterial* material)
   {
     if (!material)
       return shadingType::Unknown;
@@ -30,8 +34,10 @@ namespace hc
 
     switch (shadingModel)
     {
+    case aiShadingMode_Phong:
+      return shadingType::BlinnPhong;
     case aiShadingMode_Blinn:
-      return shadingType::Unlit;
+      return shadingType::BlinnPhong;
     case aiShadingMode_NoShading:
       return shadingType::Unlit;
     default:
@@ -44,24 +50,49 @@ namespace hc
     const aiMaterial* material
   )
   {
-    aiColor3D color(1.0f, 1.0f, 1.0f);
-    Color hcColor;
-    if (material->Get(AI_MATKEY_COLOR_DIFFUSE, color) == aiReturn_SUCCESS)
-    {
-      hcColor = Color(color.r, color.g, color.b, 1.0f);
-    }
+    return MakeShared<UnlitMaterialDescriptor>(
+      "",
+      GetVertexColorDiffuseFromMaterial(material),
+      GetTexturePathFromMaterial(fileDirectory, material, aiTextureType_DIFFUSE)
+    );
+  }
 
+  SharedPtr<AMaterialDescriptor> AssimpMaterialDescriptorParser::ParseBlinnPhongMaterialDescriptor(
+    const Path& fileDirectory,
+    const aiMaterial* material
+  )
+  {
+    return MakeShared<BlinnPhongMaterialDescriptor>(
+      "",
+      GetVertexColorDiffuseFromMaterial(material),
+      GetTexturePathFromMaterial(fileDirectory, material, aiTextureType_DIFFUSE),
+      GetTexturePathFromMaterial(fileDirectory, material, aiTextureType_NORMALS),
+      GetTexturePathFromMaterial(fileDirectory, material, aiTextureType_SPECULAR)
+    );
+  }
+
+  Color AssimpMaterialDescriptorParser::GetVertexColorDiffuseFromMaterial(
+    const aiMaterial* material
+  )
+  {
+    aiColor3D color(1.0f, 1.0f, 1.0f);
+    if (material->Get(AI_MATKEY_COLOR_DIFFUSE, color) == aiReturn_SUCCESS)
+      return Color(color.r, color.g, color.b, 1.0f);
+    return Color::White();
+  }
+
+  Path AssimpMaterialDescriptorParser::GetTexturePathFromMaterial(
+    const Path& fileDirectory,
+    const aiMaterial* material,
+    aiTextureType textureType
+  )
+  {
     aiString texturePath;
-    Path mainImagePath;
-    if (material->GetTexture(aiTextureType_DIFFUSE, 0, &texturePath) == aiReturn_SUCCESS)
+    if (material->GetTexture(textureType, 0, &texturePath) == aiReturn_SUCCESS)
     {
       Path textureRelativePath(texturePath.C_Str());
-      mainImagePath = fileDirectory / textureRelativePath;
+      return fileDirectory / textureRelativePath;
     }
-
-    SharedPtr<UnlitMaterialDescriptor> descriptor =
-      MakeShared<UnlitMaterialDescriptor>("", hcColor, mainImagePath);
-
-    return descriptor;
+    return Path();
   }
 }
