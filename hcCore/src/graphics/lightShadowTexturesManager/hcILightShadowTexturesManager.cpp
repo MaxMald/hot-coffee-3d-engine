d24c99ce4110da8638f9fba7f2f74ec0d85ffaca
Max Maldonado <nuup20@gmail.com>
2026-07-15 11:39:13 -0600

    add light shadow textures manager

---
+++ hcCore/src/graphics/lightShadowTexturesManager/hcILightShadowTexturesManager.cpp
@@ -0,0 +1,7 @@
+#include "hc/graphics/lightShadowTexturesManager/hcILightShadowTexturesManager.h"
+
+namespace hc
+{
+  ILightShadowTexturesManager::ILightShadowTexturesManager() = default;
+  ILightShadowTexturesManager::~ILightShadowTexturesManager() = default;
+}
