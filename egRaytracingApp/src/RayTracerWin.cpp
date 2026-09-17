a5ab1cd6be42af84da63bbf762e1bc42209d7a39
Max Maldonado <nuup20@gmail.com>
2026-09-17 11:49:16 -0600

    add raytracing project

---
+++ egRaytracingApp/src/RayTracerWin.cpp
@@ -0,0 +1,589 @@
+// RayTracerWin.cpp : Defines the entry point for the application.
+//
+
+#include "framework.h"
+#include "RayTracerWin.h"
+#include "MathHelpers.h"
+#include <hc/assets/hcAssetManager.h>
+
+// include directx math for vector and matrix operations
+#include  <DirectXMath.h>
+
+using namespace DirectX;
+
+#define MAX_LOADSTRING 100
+
+// Global Variables:
+HINSTANCE hInst;                                // current instance
+WCHAR szTitle[MAX_LOADSTRING];                  // The title bar text
+WCHAR szWindowClass[MAX_LOADSTRING];            // the main window class name
+
+Vector<Sphere> g_sceneSpheres; // Vector to hold the spheres in the scene
+Vector<Plane> g_scenePlanes;   // Vector to hold the planes in the scene
+hc::AssetManager g_assetManager; // Asset manager for loading assets
+
+// Forward declarations of functions included in this code module:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+// RAYTRACING 
+HitInfo FindClosestHit(const Ray& ray, REAL_TYPE minT, REAL_TYPE maxT);
+Vector4f FindColorForRay(const Ray& ray, REAL_TYPE minT, REAL_TYPE maxT, Int32 maxBounces);
+bool IntersectRaySphere(const Ray& ray, const Sphere& sphere, REAL_TYPE minT, REAL_TYPE maxT, HitInfo& hitInfo);
+bool IntersectRayPlane(const Ray& ray, const Plane& plane, REAL_TYPE minT, REAL_TYPE maxT, HitInfo& hitInfo);
+bool IsInShadow(const HitInfo& hit, const Vector3& lightPosition);
+void RaytraceScreen(HDC hdc, int width, int height);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+  _In_opt_ HINSTANCE hPrevInstance,
+  _In_ LPWSTR    lpCmdLine,
+  _In_ int       nCmdShow)
+{
+  UNREFERENCED_PARAMETER(hPrevInstance);
+  UNREFERENCED_PARAMETER(lpCmdLine);
+
+  // TODO: Place code here.
+
+  // Initialize global strings
+  LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+  LoadStringW(hInstance, IDC_RAYTRACERWIN, szWindowClass, MAX_LOADSTRING);
+  MyRegisterClass(hInstance);
+
+  // Perform application initialization:
+  if (!InitInstance(hInstance, nCmdShow))
+  {
+    return FALSE;
+  }
+
+  HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_RAYTRACERWIN));
+
+  MSG msg;
+
+  // Main message loop:
+  while (GetMessage(&msg, nullptr, 0, 0))
+  {
+    if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+    {
+      TranslateMessage(&msg);
+      DispatchMessage(&msg);
+    }
+  }
+
+  return (int)msg.wParam;
+}
+
+
+
+//
+//  FUNCTION: MyRegisterClass()
+//
+//  PURPOSE: Registers the window class.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+  WNDCLASSEXW wcex;
+
+  wcex.cbSize = sizeof(WNDCLASSEX);
+
+  wcex.style = CS_HREDRAW | CS_VREDRAW;
+  wcex.lpfnWndProc = WndProc;
+  wcex.cbClsExtra = 0;
+  wcex.cbWndExtra = 0;
+  wcex.hInstance = hInstance;
+  wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_RAYTRACERWIN));
+  wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+  wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+  wcex.lpszMenuName = MAKEINTRESOURCEW(IDC_RAYTRACERWIN);
+  wcex.lpszClassName = szWindowClass;
+  wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+  return RegisterClassExW(&wcex);
+}
+
+//
+//   FUNCTION: InitInstance(HINSTANCE, int)
+//
+//   PURPOSE: Saves instance handle and creates main window
+//
+//   COMMENTS:
+//
+//        In this function, we save the instance handle in a global variable and
+//        create and display the main program window.
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+  hInst = hInstance; // Store instance handle in our global variable
+
+  HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+    CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+
+  if (!hWnd)
+  {
+    return FALSE;
+  }
+
+  ShowWindow(hWnd, nCmdShow);
+  UpdateWindow(hWnd);
+
+  g_sceneSpheres.push_back(
+    Sphere(
+      Vector3(0.0f, 0.0f, 0.0f),
+      1.0f,
+      Color::Red(),
+      0.3,
+      0.8,
+      0.2
+    )
+  );
+
+  g_sceneSpheres.push_back(
+    Sphere(
+      Vector3(2.5f, 0.0f, 0.0f),
+      1.0f,
+      Color::Green(),
+      0.3,
+      0.8,
+      0.2
+    )
+  );
+
+  g_scenePlanes.push_back(
+    Plane(
+      Vector3(0.0f, -1.0f, 0.0f),
+      Vector3(0.0f, 1.0f, 0.0f),
+      Color::Blue(),
+      0.3,
+      0.8,
+      0.2
+    )
+  );
+
+  hc::Path rootPath = hc::Path::GetCurrentPath();
+  g_assetManager.setRootPath("assets");
+
+  return TRUE;
+}
+
+//
+//  FUNCTION: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  PURPOSE: Processes messages for the main window.
+//
+//  WM_COMMAND  - process the application menu
+//  WM_PAINT    - Paint the main window
+//  WM_DESTROY  - post a quit message and return
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+  switch (message)
+  {
+  case WM_COMMAND:
+  {
+    int wmId = LOWORD(wParam);
+    // Parse the menu selections:
+    switch (wmId)
+    {
+
+    case ID_RENDER_RENDER:
+    {
+      RECT clientRect;
+      GetClientRect(hWnd, &clientRect);
+      int width = clientRect.right - clientRect.left;
+      int height = clientRect.bottom - clientRect.top;
+      HDC hdc = GetDC(hWnd);
+      RaytraceScreen(hdc, width, height);
+      ReleaseDC(hWnd, hdc);
+    }
+    break;
+
+    case IDM_ABOUT:
+      DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+      break;
+    case IDM_EXIT:
+      DestroyWindow(hWnd);
+      break;
+    default:
+      return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+  }
+  break;
+  case WM_PAINT:
+  {
+    PAINTSTRUCT ps;
+    BeginPaint(hWnd, &ps);
+    // TODO: Add any drawing code here...
+    EndPaint(hWnd, &ps);
+  }
+  break;
+  case WM_DESTROY:
+    PostQuitMessage(0);
+    break;
+  default:
+    return DefWindowProc(hWnd, message, wParam, lParam);
+  }
+  return 0;
+}
+
+// Message handler for about box.
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+  UNREFERENCED_PARAMETER(lParam);
+  switch (message)
+  {
+  case WM_INITDIALOG:
+    return (INT_PTR)TRUE;
+
+  case WM_COMMAND:
+    if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+    {
+      EndDialog(hDlg, LOWORD(wParam));
+      return (INT_PTR)TRUE;
+    }
+    break;
+  }
+  return (INT_PTR)FALSE;
+}
+
+HitInfo FindClosestHit(const Ray& ray, REAL_TYPE minT, REAL_TYPE maxT)
+{
+  Vector<HitInfo> hitInfos;
+
+  HitInfo closestHitInfo;
+  float closestDistance = maxT;
+
+  // Determining the closest intersection with any sphere in the scene
+  for (const auto& sphere : g_sceneSpheres)
+  {
+    HitInfo hitCandidate;
+    if (IntersectRaySphere(ray, sphere, minT, maxT, hitCandidate))
+    {
+      if (hitCandidate.distance < closestDistance)
+      {
+        closestDistance = hitCandidate.distance;
+        closestHitInfo = hitCandidate;
+        closestHitInfo.pSphere = const_cast<Sphere*>(&sphere);
+        closestHitInfo.pPlane = nullptr;
+      }
+    }
+  }
+
+  // Determining the closest intersection with any plane in the scene
+  for (const auto& plane : g_scenePlanes)
+  {
+    HitInfo hitCandidate;
+    if (IntersectRayPlane(ray, plane, minT, maxT, hitCandidate))
+    {
+      if (hitCandidate.distance < closestDistance)
+      {
+        closestDistance = hitCandidate.distance;
+        closestHitInfo = hitCandidate;
+        closestHitInfo.pPlane = const_cast<Plane*>(&plane);
+        closestHitInfo.pSphere = nullptr;
+      }
+    }
+  }
+
+  return closestHitInfo;
+}
+
+Vector4f FindColorForRay(
+  const Ray& ray,
+  REAL_TYPE minT,
+  REAL_TYPE maxT,
+  Int32 maxBounces
+)
+{
+  const Vector3 lightPosition(5.0f, 5.0f, -5.0f);
+  const Vector4f lightColor(1.0f, 1.0f, 1.0f, 1.0f);
+  const Vector4f backgroundColor(0.5f, 0.7f, 1.0f, 1.0f); // Light blue background
+
+  // Radiance accumulation
+  Vector4f radiance(0.0f, 0.0f, 0.0f, 1.0f); // Initialize radiance to black
+
+  // Ammount of energy that keeps traveling after each bounce
+  Vector4f throughput(1.0f, 1.0f, 1.0f, 1.0f); // Initialize throughput to white
+
+  int depth = 0;
+  Ray currentRay = ray;
+
+  while (depth < maxBounces)
+  {
+    HitInfo closestHitInfo = FindClosestHit(currentRay, minT, maxT);
+    if (closestHitInfo.pSphere == nullptr && closestHitInfo.pPlane == nullptr)
+    {
+      radiance = radiance + (throughput * backgroundColor); // Accumulate radiance
+      break;
+    }
+
+    Vector4f objectColor(0.0f, 0.0f, 0.0f, 1.0f);
+    if (closestHitInfo.pSphere)
+    {
+      const Sphere& sphere = *closestHitInfo.pSphere;
+      objectColor = sphere.color.vec4;
+    }
+    else if (closestHitInfo.pPlane)
+    {
+      const Plane& plane = *closestHitInfo.pPlane;
+      objectColor = plane.color.vec4;
+    }
+
+    constexpr REAL_TYPE ambientFactor = 0.05f; // Ambient light factor
+    constexpr REAL_TYPE diffuseFactor = 0.95f; // Diffuse reflection factor
+    constexpr REAL_TYPE shininess = 32.0f; // Shininess factor for specular reflection
+    REAL_TYPE reflectivity = 0.3f; // Specular coefficient
+
+    if (closestHitInfo.pPlane != nullptr)
+      reflectivity = 0.0f;
+
+    const Vector3 lightDir = (lightPosition - closestHitInfo.position).normalized();
+    const Vector3 viewDir = currentRay.direction.normalized() * -1.0f;
+    const Vector3 halfVector = (lightDir + viewDir).normalized();
+
+    const REAL_TYPE NdL = max(closestHitInfo.normal.dot(lightDir), 0.0f); // Lambertian reflection
+    const REAL_TYPE NdH = max(closestHitInfo.normal.dot(halfVector), 0.0f); // Blinn-Phong reflection
+
+    REAL_TYPE visibility = 0.0f;
+    if (NdL > 0.0f)
+    {
+      visibility = IsInShadow(closestHitInfo, lightPosition) ? 0.0f : 1.0f;
+    }
+
+    const REAL_TYPE diffuse = ambientFactor + diffuseFactor * NdL; // Diffuse
+    const REAL_TYPE specular = powf(NdH, shininess); // Specular
+
+    const Vector4f diffuseColor = objectColor * diffuse;
+    const Vector4f specularColor = lightColor * specular;
+
+    const REAL_TYPE localWeight = 1.0f - reflectivity; // Weight for local color contribution
+    radiance += throughput * (diffuseColor + specularColor) * localWeight * visibility; // Accumulate radiance
+    throughput *= reflectivity; // Update throughput for the next bounce
+
+    const REAL_TYPE remainingEnergy = max(throughput.x, max(throughput.y, throughput.z));
+    if (remainingEnergy < 0.001f)
+    {
+      break; // Terminate the loop if the remaining energy is very low
+    }
+
+    Vector3 reflectedDir = currentRay.direction - 2.0f * closestHitInfo.normal * currentRay.direction.dot(closestHitInfo.normal);
+    constexpr REAL_TYPE epsilon = 1e-4f; // Small offset to avoid self-intersection
+    currentRay = Ray(closestHitInfo.position + reflectedDir * epsilon, reflectedDir); // Offset to avoid self-intersection
+    depth++;
+  }
+
+  return radiance;
+}
+
+bool IntersectRaySphere(
+  const Ray& ray,
+  const Sphere& sphere,
+  REAL_TYPE minT,
+  REAL_TYPE maxT,
+  HitInfo& hitInfo
+)
+{
+  Vector3 originToCenter = ray.origin - sphere.center;
+
+  float a = ray.direction.dot(ray.direction); // squared length of the direction vector
+  float b = 2.0f * originToCenter.dot(ray.direction); // 2 times the dot product of the vector from the ray origin to the sphere center and the ray direction
+  float c = originToCenter.dot(originToCenter) - (sphere.radius * sphere.radius); // squared length of the vector from the ray origin to the sphere center minus the squared radius of the sphere
+
+  // Calculate the discriminant of the quadratic equation to determine if there are real
+  // roots (i.e., if the ray intersects the sphere)
+  float discriminant = b * b - 4 * a * c; // discriminant of the quadratic equation
+
+  // If the discriminant is negative, there are no real roots, meaning the ray does not
+  // intersect the sphere
+  if (discriminant < 0)
+    return false;
+
+  // Calculate the two possible intersection points (t1 and t2) using the quadratic
+  // formula
+  float sqrtDiscriminant = sqrtf(discriminant);
+  float inverseDenominator = 1.0f / (2.0f * a);
+  float distance = (-b - sqrtDiscriminant) * inverseDenominator;
+
+  if (distance < minT || distance > maxT)
+  {
+    distance = (-b + sqrtDiscriminant) * inverseDenominator;
+    if (distance < minT || distance > maxT)
+      return false; // Out of bounds, no valid intersection within the specified range
+  }
+
+  Vector3 position = ray.where(distance);
+  Vector3 normal = (position - sphere.center).normalized();
+
+  hitInfo.distance = distance;
+  hitInfo.position = position;
+  hitInfo.normal = normal;
+
+  return true;
+}
+
+bool IntersectRayPlane(
+  const Ray& ray,
+  const Plane& plane,
+  REAL_TYPE minT,
+  REAL_TYPE maxT,
+  HitInfo& hitInfo
+)
+{
+  const REAL_TYPE denomitator = plane.normal.dot(ray.direction);
+
+  // check if the ray is parallel to the plane
+  if (fabs(denomitator) < 1e-4f)
+    return false;
+
+  const REAL_TYPE distance = (plane.point - ray.origin).dot(plane.normal) / denomitator;
+  if (distance < minT || distance > maxT)
+    return false;
+
+  hitInfo.distance = distance;
+  hitInfo.position = ray.where(distance);
+  hitInfo.normal = plane.normal;
+
+  return true;
+}
+
+bool IsInShadow(const HitInfo& hit, const Vector3& lightPosition)
+{
+  const REAL_TYPE shadowBias = REAL_TYPE(0.001);
+  Ray shadowRay;
+  shadowRay.origin = hit.position + hit.normal * shadowBias;
+
+  Vector3 toLight = lightPosition - shadowRay.origin;
+
+  const REAL_TYPE lightDistance = toLight.length();
+  if (lightDistance <= shadowBias * REAL_TYPE(2))
+    return false;
+
+  shadowRay.direction = toLight * (REAL_TYPE(1) / lightDistance);
+  const HitInfo shadowHit = FindClosestHit(shadowRay, shadowBias, lightDistance);
+  return (shadowHit.pSphere != nullptr || shadowHit.pPlane != nullptr);
+}
+
+void RaytraceScreen(HDC hdc, int width, int height)
+{
+  // Camera
+  const Vector3 eyePosition = Vector3(0.0f, 1.0f, -5.0f);
+  const Vector3 camTarget = Vector3(0.0f, 0.0f, 0.0f);
+  const Vector3 camUp = Vector3(0.0f, 1.0f, 0.0f);
+
+  const Vector3 forward = (camTarget - eyePosition).normalized();
+  const Vector3 right = camUp.cross(forward).normalized();
+  const Vector3 up = forward.cross(right);
+
+  // Projection paramaters
+  constexpr float verticalFOV = XMConvertToRadians(60.0f);
+  const float nearPlane = 0.1f;
+  const float farPlane = 100.0f;
+  const float aspectRatio = static_cast<float>(width) / static_cast<float>(height);
+
+  const float  halfHeight = tanf(verticalFOV / 2.0f);
+  const float halfWidth = aspectRatio * halfHeight;
+
+  const float minRayDistance = nearPlane;
+  const float maxRayDistance = farPlane;
+
+  // VERSION: antisampling version + parallelization
+
+  float samplesPerAxis = 2.0f;
+  float samplesPerPixel = samplesPerAxis * samplesPerAxis;
+
+  Vector<Vector4f> pixelColors(width * height, Vector4f(0.0f, 0.0f, 0.0f, 1.0f));
+
+  // parallelization for the outer loop (y-axis), which holds the most independent
+  // iterations, to avoid race conditions when writing to pixelColors. Each thread will
+  // work on different rows of pixels, so there won't be any conflicts when writing to
+  // the pixelColors vector.
+#pragma omp parallel for 
+
+  for (int y = 0; y < height; ++y)
+  {
+    for (int x = 0; x < width; ++x)
+    {
+      Vector4f finalColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+      for (int sy = 0; sy < samplesPerAxis; ++sy)
+      {
+        for (int sx = 0; sx < samplesPerAxis; ++sx)
+        {
+          const float offsetX = (sy + 0.5f) / samplesPerAxis;
+          const float offsetY = (sx + 0.5f) / samplesPerAxis;
+
+          const float pixelX = static_cast<float>(x) + offsetX;
+          const float pixelY = static_cast<float>(y) + offsetY;
+
+          // PIXELES NDC COORDINATS
+
+          const float ndcX = (pixelX / static_cast<float>(width)) * 2.0f - 1.0f;
+          const float ndcY = 1.0f - (pixelY / static_cast<float>(height) * 2.0f); // Invert Y axis for screen space, direct x coordinates
+
+          // Posición sobre el plano de la imagen local
+          const float cameraX = ndcX * halfWidth;
+          const float cameraY = ndcY * halfHeight;
+
+          // Dirección del rayo en el espacio del mundo
+          Vector3 rayDirection = ((right * cameraX) + (up * cameraY) + forward).normalized();
+
+          Ray ray(eyePosition, rayDirection);
+          finalColor = finalColor + FindColorForRay(ray, minRayDistance, maxRayDistance, 3);
+        }
+      }
+
+      finalColor = finalColor / samplesPerPixel;
+
+      const UInt8 R = min(finalColor.x * 255.0f, 255.0f);
+      const UInt8 G = min(finalColor.y * 255.0f, 255.0f);
+      const UInt8 B = min(finalColor.z * 255.0f, 255.0f);
+      //SetPixel(hdc, x, y, RGB(R, G, B));
+      pixelColors[y * width + x] = finalColor;
+    }
+  }
+
+  for (int y = 0; y < height; ++y)
+  {
+    for (int x = 0; x < width; ++x)
+    {
+      const Vector4f& finalColor = pixelColors[y * width + x];
+      const UInt8 R = min(finalColor.x * 255.0f, 255.0f);
+      const UInt8 G = min(finalColor.y * 255.0f, 255.0f);
+      const UInt8 B = min(finalColor.z * 255.0f, 255.0f);
+      SetPixel(hdc, x, y, RGB(R, G, B));
+    }
+  }
+
+  // VERSION: Normal version, no antisamples and no parallelization
+
+  /*
+  for (int y = 0; y < height; ++y)
+  {
+    for (int x = 0; x < width; ++x)
+    {
+      const float pixelX = static_cast<float>(x) + 0.5f;
+      const float pixelY = static_cast<float>(y) + 0.5f;
+
+      // PIXELES NDC COORDINATS
+
+      const float ndcX = (pixelX / static_cast<float>(width)) * 2.0f - 1.0f;
+      const float ndcY = 1.0f - (pixelY / static_cast<float>(height) * 2.0f); // Invert Y axis for screen space, direct x coordinates
+
+      // Posición sobre el plano de la imagen local
+      const float cameraX = ndcX * halfWidth;
+      const float cameraY = ndcY * halfHeight;
+
+      // Dirección del rayo en el espacio del mundo
+      Vector3 rayDirection = ((right * cameraX) + (up * cameraY) + forward).normalized();
+
+      Ray ray(eyePosition, rayDirection);
+      Vector4 finalColor = FindColorForRay(ray, minRayDistance, maxRayDistance, 3);
+
+      const UInt8 R = min(finalColor.x * 255.0f, 255.0f);
+      const UInt8 G = min(finalColor.y * 255.0f, 255.0f);
+      const UInt8 B = min(finalColor.z * 255.0f, 255.0f);
+      SetPixel(hdc, x, y, RGB(R, G, B));
+    }
+  }*/
+}
